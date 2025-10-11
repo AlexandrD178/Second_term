d410abf722e10740166f3694e70107e7f6e8240b
AlexandrD178 <dirun2005@mail.ru>
2025-10-11 21:42:11 +0300

    Gorizonts commit

---
+++ INR/build/CMakeFiles/my_lib.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alexd/Downloads/Second_term/INR/Collider.cpp" "CMakeFiles/my_lib.dir/Collider.cpp.o" "gcc" "CMakeFiles/my_lib.dir/Collider.cpp.o.d"
+  "/home/alexd/Downloads/Second_term/INR/GlauNucleon.cpp" "CMakeFiles/my_lib.dir/GlauNucleon.cpp.o" "gcc" "CMakeFiles/my_lib.dir/GlauNucleon.cpp.o.d"
+  "/home/alexd/Downloads/Second_term/INR/GlauNucleus.cpp" "CMakeFiles/my_lib.dir/GlauNucleus.cpp.o" "gcc" "CMakeFiles/my_lib.dir/GlauNucleus.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
